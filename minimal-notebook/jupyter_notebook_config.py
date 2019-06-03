f983d1be8aacb7b7c5f9b31ca7d6c7e6b93c0168
Unknown <guillaume.moutier@gmail.com>
2019-06-03 06:50:18 -0400

    Prevent from crashing when no key

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -25,8 +25,9 @@ from s3contents import S3ContentsManager
 c = get_config()
 
 # Tell Jupyter to use S3ContentsManager for all storage.
-c.NotebookApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-c.S3ContentsManager.secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-c.S3ContentsManager.endpoint_url = os.environ.get("S3_ENPOINT_URL")
-c.S3ContentsManager.bucket = 'valeria-users-' + os.environ.get("JUPYTERHUB_USER")
+if os.environ.get("AWS_ACCESS_KEY_ID"):
+    c.NotebookApp.contents_manager_class = S3ContentsManager
+    c.S3ContentsManager.access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+    c.S3ContentsManager.secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    c.S3ContentsManager.endpoint_url = os.environ.get("S3_ENPOINT_URL")
+    c.S3ContentsManager.bucket = 'valeria-users-' + os.environ.get("JUPYTERHUB_USER")
