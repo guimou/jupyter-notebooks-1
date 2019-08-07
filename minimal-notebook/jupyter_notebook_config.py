29f7146744610748c3c36c46c6c5ecf5cd8bcb74
Unknown <guillaume.moutier@gmail.com>
2019-08-07 15:27:43 -0400

    check AWS key not 'none'

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -25,7 +25,7 @@ from s3contents import S3ContentsManager
 c = get_config()
 
 # Tell Jupyter to use S3ContentsManager for all storage.
-if os.environ.get("AWS_ACCESS_KEY_ID"):
+if os.environ.get("AWS_ACCESS_KEY_ID")!='none':
     c.NotebookApp.contents_manager_class = S3ContentsManager
     c.S3ContentsManager.access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
     c.S3ContentsManager.secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
