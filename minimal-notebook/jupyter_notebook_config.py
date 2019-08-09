f91811bd0b1f082f9c4af395a894ab71be87f4a4
Unknown <guillaume.moutier@gmail.com>
2019-08-09 13:51:10 -0400

    hybrid content manager and update packages

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -1,5 +1,7 @@
 import os
 
+c = get_config()
+
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8080
 c.NotebookApp.open_browser = False
@@ -19,15 +21,41 @@ if os.path.exists(image_config_file):
         exec(compile(fp.read(), image_config_file, 'exec'), globals())
 
 
-# S3 configuration
+# directories configuration
 from s3contents import S3ContentsManager
+from pgcontents.hybridmanager import HybridContentsManager
+from notebook.services.contents.filemanager import FileContentsManager
 
-c = get_config()
+c.NotebookApp.contents_manager_class = HybridContentsManager
 
-# Tell Jupyter to use S3ContentsManager for all storage.
+c.HybridContentsManager.manager_classes = {
+    # Associate the root directory with a FileContentsManager.
+    # This manager will receive all requests that don't fall under any of the
+    # other managers.
+    '': FileContentsManager,
+    # Associate /directory with a S3ContentsManager.
+    'datalake': S3ContentsManager
+}
+
+c.HybridContentsManager.manager_kwargs = {
+    # Args for the FileContentsManager mapped to /directory
+    'directory': {
+        'root_dir': '/users'
+    },
+
+    # S3ContentsManager
+    'data': {
+        'access_key_id': os.environ.get("AWS_ACCESS_KEY_ID"),
+        'secret_access_key': os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        'endpoint_url': os.environ.get("S3_ENDPOINT_URL"),
+        'bucket': 'valeria-users-' + os.environ.get("JUPYTERHUB_USER")
+    }
+}
+
+""" # Tell Jupyter to use S3ContentsManager for all storage.
 if os.environ.get("AWS_ACCESS_KEY_ID")!='none':
     c.NotebookApp.contents_manager_class = S3ContentsManager
     c.S3ContentsManager.access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
     c.S3ContentsManager.secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
     c.S3ContentsManager.endpoint_url = os.environ.get("S3_ENDPOINT_URL")
-    c.S3ContentsManager.bucket = 'valeria-users-' + os.environ.get("JUPYTERHUB_USER")
+    c.S3ContentsManager.bucket = 'valeria-users-' + os.environ.get("JUPYTERHUB_USER") """
